a693bffef115283bc387c7d48608655225ad68b2
corey <corey101@qq.com>
2022-05-07 17:30:17 +0800

    SQL性能分析插件移除

--- service/service-acl/src/main/java/com/atguigu/aclservice/config/MyBatisPlusConfig.java
+++ service/service-acl/src/main/java/com/atguigu/aclservice/config/MyBatisPlusConfig.java
@@ -19,18 +19,18 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.atguigu.aclservice.mapper")
 public class MyBatisPlusConfig {
 
-    /**
-     * SQL 执行性能分析插件
-     * 开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长
-     */
-    @Bean
-    @Profile({"dev","test"})// 设置 dev test 环境开启
-    public PerformanceInterceptor performanceInterceptor() {
-        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
-        performanceInterceptor.setMaxTime(1000);//ms，超过此处设置的ms则sql不执行
-        performanceInterceptor.setFormat(true);
-        return performanceInterceptor;
-    }
+//    /**
+//     * SQL 执行性能分析插件
+//     * 开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长
+//     */
+//    @Bean
+//    @Profile({"dev","test"})// 设置 dev test 环境开启
+//    public PerformanceInterceptor performanceInterceptor() {
+//        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+//        performanceInterceptor.setMaxTime(1000);//ms，超过此处设置的ms则sql不执行
+//        performanceInterceptor.setFormat(true);
+//        return performanceInterceptor;
+//    }
 
     /**
      * 逻辑删除插件
